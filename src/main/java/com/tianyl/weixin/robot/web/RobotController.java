172198fb03145519f930ebe739de17dec3714841
tianyale <tianyale@ufenqi.com>
2016-07-08 17:39:52 +0800

    init project

---
+++ src/main/java/com/tianyl/weixin/robot/web/RobotController.java
@@ -0,0 +1,12 @@
+package com.tianyl.weixin.robot.web;
+
+import com.tianyl.core.mvc.annotation.Controller;
+
+@Controller("/robot")
+public class RobotController {
+
+	public Object m1() {
+		return "OK";
+	}
+
+}
